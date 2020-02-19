e1a3b4462484d8d58d6323959b3f379ab4dc0e19
amrmynd <ariley.amy@gmail.com>
2020-02-19 17:44:00 +0100

    adding form validation for user registration using express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 const userModel = require("../model/userModel");
 const HttpError = require("../model/http-error");
@@ -7,42 +8,58 @@ const HttpError = require("../model/http-error");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-router.post("/", async (req, res, next) => {
-  const { email, password, profilePicture } = req.body;
+router.post(
+  "/",
+  [check("email").isEmail(), check("password").isLength({ min: 5 })],
+  async (req, res, next) => {
+    const { email, password, profilePicture } = req.body;
 
-  var hash = bcrypt.hashSync(password, saltRounds);
+    var hash = bcrypt.hashSync(password, saltRounds);
 
-  let existingUser;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new HttpError(
+        "Invalid inputs passed, please check your data.",
+        422
+      );
+      return next(error);
+    }
 
-  try {
-    existingUser = await userModel.findOne({ email: email });
-  } catch (err) {
-    const error = new HttpError("Signup failed, please try again later.", 500);
-    return next(error);
-  }
+    let existingUser;
 
-  if (existingUser) {
-    const error = new HttpError(
-      "User already exists. Please choose another email.",
-      422
-    );
-    return next(error);
-  }
+    try {
+      existingUser = await userModel.findOne({ email: email });
+    } catch (err) {
+      const error = new HttpError(
+        "Signup failed, please try again later.",
+        500
+      );
+      return next(error);
+    }
 
-  const newUser = new userModel({
-    email: email,
-    password: hash,
-    profilePicture: profilePicture
-  });
+    if (existingUser) {
+      const error = new HttpError(
+        "User already exists. Please choose another email.",
+        422
+      );
+      return next(error);
+    }
 
-  newUser
-    .save()
-    .then(user => {
-      res.send(user);
-    })
-    .catch(err => {
-      res.status(500).send("Server Error");
+    const newUser = new userModel({
+      email: email,
+      password: hash,
+      profilePicture: profilePicture
     });
-});
+
+    newUser
+      .save()
+      .then(user => {
+        res.send(user);
+      })
+      .catch(err => {
+        res.status(500).send("Server Error");
+      });
+  }
+);
 
 module.exports = router;
